30e4432eafc159fa8180d91685a92f6009725bec
JasonDeee <leesama2k@gmail.com>
2021-10-14 21:56:14 +0700

    sasafsdf

--- map.js
+++ map.js
@@ -1,7 +1,15 @@
+const MapsInit = document.createElement("script");
+
+MapsInit.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDI1Bt5fcHnAeU5hc1Q6yswi6qwe3N0P3w&callback=initMap&v=weekly";
+MapsInit.async = true;
+
+document.body.appendChild(MapsInit);
+
 const initMap = () => {
   new google.maps.Map(document.getElementById("map"), {
-    mapId: "8e0a97af9386fef",
+    mapId: "99f215073d577b09",
     center: { lat: 48.85, lng: 2.35 },
-    zoom: 12,
+    zoom: 32,
   });
 };
