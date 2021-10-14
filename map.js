60a3c526dc1cd2ff17c320eaca47bf68abaeb1b2
JasonDeee <leesama2k@gmail.com>
2021-10-14 21:38:44 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,18 +1,18 @@
-const Map_Header = document.querySelector("#Map_Header");
-
-const Mapscript = Map_Header.createElement("script");
+const Mapscript = document.createElement("script");
 
 Mapscript.src =
   "https://maps.googleapis.com/maps/api/js?key=AIzaSyDI1Bt5fcHnAeU5hc1Q6yswi6qwe3N0P3w&callback=initMap&v=weekly";
 Mapscript.async = true;
 
-Map_Header.body.appendChild(Mapscript);
+document.body.appendChild(Mapscript);
 //
 
-const initMap = () => {
-  new google.maps.Map(document.getElementById("VDFM_map"), {
+function initMap() {
+  new google.maps.Map(document.getElementById("map"), {
     mapId: "99f215073d577b09",
     center: { lat: 48.85, lng: 2.35 },
     zoom: 8,
   });
-};
+}
+
+initMap();
