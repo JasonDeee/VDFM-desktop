666766839c92a49ff64b33fadc3b7a943230796e
JasonDeee <leesama2k@gmail.com>
2021-10-14 21:23:39 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const initMap = () => {
   new google.maps.Map(document.getElementById("map"), {
-    mapId: "8e0a97af9386fef",
+    mapId: "99f215073d577b09",
     center: { lat: 48.85, lng: 2.35 },
     zoom: 12,
   });
