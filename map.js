4a10ad72b6e5799d95fa18c6858369f00a368c9f
JasonDeee <leesama2k@gmail.com>
2021-10-14 21:35:51 +0700

    Update map.js

--- map.js
+++ map.js
@@ -13,6 +13,6 @@ const initMap = () => {
   new google.maps.Map(document.getElementById("VDFM_map"), {
     mapId: "99f215073d577b09",
     center: { lat: 48.85, lng: 2.35 },
-    zoom: 12,
+    zoom: 8,
   });
 };
