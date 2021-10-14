69481ff44f9e21192f3edaf763be9e775bee3870
JasonDeee <leesama2k@gmail.com>
2021-10-14 22:09:30 +0700

    V1.2

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
-function initMap() {
-  new google.maps.Map(document.getElementById("VDFM_map"), {
-    mapId: "99f215073d577b09",
+const initMap = () => {
+  new google.maps.Map(document.getElementById("map"), {
+    mapId: "8e0a97af9386fef",
     center: { lat: 48.85, lng: 2.35 },
-    zoom: 8,
+    zoom: 12,
   });
-}
+};
