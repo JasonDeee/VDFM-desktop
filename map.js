8fc39761d8d1e1da37444293bd4f90167b51fc99
JasonDeee <leesama2k@gmail.com>
2021-10-14 21:29:30 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,10 @@
+Mapscript.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDI1Bt5fcHnAeU5hc1Q6yswi6qwe3N0P3w&callback=initMap&v=weekly";
+Mapscript.async = true;
+
+document.body.appendChild(Mapscript);
+//
+
 const initMap = () => {
   new google.maps.Map(document.getElementById("map"), {
     mapId: "99f215073d577b09",
