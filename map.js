63f45b3dc875de74badc285b3a0c3b738cddafd6
JasonDeee <leesama2k@gmail.com>
2021-10-15 00:20:32 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,49 @@
+//
+// 20.980385065798963, 105.78844061609924 - NIK Tòa Sông Đà
+// TẦNG 06, TÒA NHÀ SÔNG ĐÀ 11 131, Hà Đông, Việt Nam
+//
+//
+// 20.980597227612375, 105.78793084435914
+// Học viện Công nghệ Bưu chính viễn thông - LATIDUTE & LONGITUDE location
+//
+// 20.980810845015128, 105.78716922432768 - Center Map location
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("VDFM_map"), {
+    mapId: "99f215073d577b09",
+    center: { lat: 20.980810845015128, lng: 105.78716922432768 },
+    zoom: 18,
+    fullscreenControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+  });
+
+  const marker = new google.maps.Marker({
+    position: { lat: 20.980385065798963, lng: 105.78844061609924 },
+    map,
+    title: "VDFM Hanoi",
+    icon: {
+      url: "https://alpha.vdfm.ga/assets/VDFM_MapIcon.svg",
+      scaledSize: new google.maps.Size(62, 87),
+    },
+    animation: google.maps.Animation.DROP,
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: "VDFM Hanoi",
+  });
+
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  });
+}
+//
+//
+//
 window.google = window.google || {};
 google.maps = google.maps || {};
 (function () {
@@ -8472,47 +8518,3 @@ Dual licensed under the MIT and GPL licenses.
 }.call(this, {}));
 //
 //
-
-//
-// 20.980385065798963, 105.78844061609924 - NIK Tòa Sông Đà
-// TẦNG 06, TÒA NHÀ SÔNG ĐÀ 11 131, Hà Đông, Việt Nam
-//
-//
-// 20.980597227612375, 105.78793084435914
-// Học viện Công nghệ Bưu chính viễn thông - LATIDUTE & LONGITUDE location
-//
-// 20.980810845015128, 105.78716922432768 - Center Map location
-
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("VDFM_map"), {
-    mapId: "99f215073d577b09",
-    center: { lat: 20.980810845015128, lng: 105.78716922432768 },
-    zoom: 18,
-    fullscreenControl: false,
-    streetViewControl: false,
-    mapTypeControl: false,
-  });
-
-  const marker = new google.maps.Marker({
-    position: { lat: 20.980385065798963, lng: 105.78844061609924 },
-    map,
-    title: "VDFM Hanoi",
-    icon: {
-      url: "https://alpha.vdfm.ga/assets/VDFM_MapIcon.svg",
-      scaledSize: new google.maps.Size(62, 87),
-    },
-    animation: google.maps.Animation.DROP,
-  });
-
-  const infowindow = new google.maps.InfoWindow({
-    content: "VDFM Hanoi",
-  });
-
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
-      map,
-      shouldFocus: false,
-    });
-  });
-}
