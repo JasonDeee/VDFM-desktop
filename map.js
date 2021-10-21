9ee99ac98f79b5192d5fe1560cf6e2215f7a4445
JasonDeee <leesama2k@gmail.com>
2021-10-21 22:35:27 +0700

    Update map.js

--- map.js
+++ map.js
@@ -8,47 +8,49 @@
 //
 // 20.980810845015128, 105.78716922432768 - Center Map location
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("VDFM_map"), {
-    mapId: "99f215073d577b09",
-    center: { lat: 20.980810845015128, lng: 105.78716922432768 },
-    zoom: 18,
-    fullscreenControl: false,
-    streetViewControl: false,
-    mapTypeControl: false,
-  });
+//
+const Mapscripts = document.createElement("script");
 
-  const marker = new google.maps.Marker({
-    position: { lat: 20.980385065798963, lng: 105.78844061609924 },
-    map,
-    title: "VDFM Hanoi",
-    icon: {
-      url: "https://alpha.vdfm.ga/assets/VDFM_MapIcon.svg",
-      scaledSize: new google.maps.Size(62, 87),
-    },
-    animation: google.maps.Animation.DROP,
-  });
+Mapscripts.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDI1Bt5fcHnAeU5hc1Q6yswi6qwe3N0P3w&callback=initMap&v=weekly";
+Mapscripts.async = true;
+document.body.appendChild(Mapscripts);
 
-  const infowindow = new google.maps.InfoWindow({
-    content: "VDFM Hanoi",
-  });
-
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
-      map,
-      shouldFocus: false,
+Mapscripts.addEventListener("load", () => {
+  function initMap() {
+    const map = new google.maps.Map(document.getElementById("VDFM_map"), {
+      mapId: "99f215073d577b09",
+      center: { lat: 20.980810845015128, lng: 105.78716922432768 },
+      zoom: 18,
+      fullscreenControl: false,
+      streetViewControl: false,
+      mapTypeControl: false,
     });
-  });
-}
-//
-//
-//
-var scriptjs = require("scriptjs");
 
-scriptjs(
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDI1Bt5fcHnAeU5hc1Q6yswi6qwe3N0P3w&callback=initMap&v=weekly",
-  function () {
-    L.mapbox.accessToken = "MyToken";
+    const marker = new google.maps.Marker({
+      position: { lat: 20.980385065798963, lng: 105.78844061609924 },
+      map,
+      title: "VDFM Hanoi",
+      icon: {
+        url: "https://alpha.vdfm.ga/assets/VDFM_MapIcon.svg",
+        scaledSize: new google.maps.Size(62, 87),
+      },
+      animation: google.maps.Animation.DROP,
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+      content: "VDFM Hanoi",
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: false,
+      });
+    });
   }
-);
+  //
+  //
+  //
+});
