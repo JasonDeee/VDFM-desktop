6bb1aa66c54553767a05c63d7419d65aa78a8fed
JasonDeee <leesama2k@gmail.com>
2021-10-28 17:53:39 +0700

    Update frameReszie.js

--- frameReszie.js
+++ frameReszie.js
@@ -1,10 +1,9 @@
 const theFrame = document.querySelector(".MainNews iframe");
 
 function ResizeIframe() {
-  console.log(theFrame.contentWindow.document.documentElement.scrollHeight);
+  console.log(theFrame.contentWindow.document.body.clientHeight);
   theFrame.style.height =
-    theFrame.contentWindow.document.documentElement.scrollHeight + "px";
+    theFrame.contentWindow.document.document.body.clientHeight + "px";
 }
 ResizeIframe();
-
 window.addEventListener("resize", ResizeIframe);
